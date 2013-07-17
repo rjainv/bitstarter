038c7c1927c1006692e944541495fc390f7dd555
Rahul Jain <rjainv@gmail.com>
2013-07-18 02:09:43 +0530

    add index.html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var body = new Buffer( fs.readFileSync("./index.html"));
+  //response.send('Hello World 2');
+  //response.writeHead(200, {"Content-Type": "text/plain"});
+  //console.log(buffer);
+  //response.send(buffer);
+
+  response.setHeader('Content-Type', 'text/html');
+  response.setHeader('Content-Length', body.length);
+  response.end(body);
+
 });
 
 var port = process.env.PORT || 5000;
